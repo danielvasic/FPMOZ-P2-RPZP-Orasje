4107bb70f1d8a9df06645e0d1cfe2f1af579bee5
danielvasic <dvasic1@gmail.com>
2020-05-13 13:05:34 +0200

    Vježbe 13052020

--- VJ05.py
+++ VJ05.py
@@ -14,9 +14,28 @@ class Biciklo(PrevoznoSredstvo):
     def vozi (self):
         return "Ja sam biciklo i vozim se na dva točka..."
 
+class Osoba:
+    def __init__(self, ime, prezime):
+        self.ime = ime
+        self.prezime = prezime
+
+    def __str__(self):
+        return self.ime + " " + self.prezime
+
 class Jahta(PrevoznoSredstvo):
     def __init__(self, naziv, tip):
         super().__init__(naziv, tip)
+        self.posada = []
+
+    def dodaj_clan_posade (self, osoba):
+        self.posada.append(osoba)
+
+    def __iter__(self):
+        self.posada = iter(self.posada)
+        return self.posada
+
+    def __next__(self):
+        next(self.posada)
 
     def vozi (self):
         return "Ja sam Jahta i vozim se po moru..."
@@ -38,3 +57,15 @@ if __name__=="__main__":
 
     for prevoznoSredstvo in listaPrevoznihSredstava:
         print(prevoznoSredstvo.vozi())
+
+    # Dodavanje iteracija kroz kolekcijske klase
+
+    jahta.dodaj_clan_posade(Osoba("Pero", "Peric"))
+    jahta.dodaj_clan_posade(Osoba("Mate", "Matic"))
+    jahta.dodaj_clan_posade(Osoba("Ana", "Anic"))
+
+    jahta = iter(jahta)
+
+    for osoba in jahta:
+        print(osoba)
+
